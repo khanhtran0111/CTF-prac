93bfe75eaae22619ff715b9956234d62dbbf5124
khanhtran0111 <giakhanhcnh@gmail.com>
2024-08-05 17:12:13 +0700

    Update p2.py

--- P2/p2.py
+++ P2/p2.py
@@ -4,6 +4,5 @@ with open('d:/Coding/Practice CTF/CTF-prac/P2/p2.txt', 'r') as file:
     data = file.read()
 
 data = data.encode()
-
-data = bytes.fromhex(data.decode())
+data = data.encode()
 print(data)
