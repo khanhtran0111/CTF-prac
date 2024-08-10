d50cd25fc615bd7eea9206064bc462220ffa1ef7
khanhtran0111 <giakhanhcnh@gmail.com>
2024-08-10 17:51:47 +0700

    fixed

--- picoCTF/interencdec.py
+++ picoCTF/interencdec.py
@@ -1,12 +1,24 @@
 import string
 import base64
+def decrypt_flag(flag):
+    decrypted_flag = ""
+    for char in flag:
+        if char.isalpha() and char.islower():
+            decrypted_flag += chr((ord(char) - 7 - ord('a')) % 26 + ord('a'))
+        elif char.isalpha() and char.isupper():
+            decrypted_flag += chr((ord(char) - 7 - ord('A')) % 26 + ord('A'))
+        else:
+            decrypted_flag += char
+    return decrypted_flag
 
 with open('D:/Coding/Practice CTF/CTF-prac/picoCTF/enc_flag.txt', 'r') as file:
     data = file.read()
     file.close()
 
-#data = data.encode()
-#data = bytes.fromhex(data.decode())
-data = base64.b64decode(data)
-data = bytes.fromhex(data)
-print(data)
+decode_bytes = base64.b64decode(data)
+flag = decode_bytes.decode('utf-8')
+print(flag)
+
+#flag = "wpjvJAM{jhlzhy_k3jy9wa3k_h47j6k69}"
+decrypted_flag = decrypt_flag(flag)
+print(decrypted_flag)
