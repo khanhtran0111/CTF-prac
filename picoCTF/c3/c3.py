a6ef47603c19ce27eb3407416e699e3b22b5dd49
Tran Gia Khanh <giakhanhcnh@gmail.com>
2024-10-14 11:23:31 +0700

    Update

--- picoCTF/c3/c3.py
+++ picoCTF/c3/c3.py
@@ -1,6 +1,6 @@
-with open('D:/Coding/CTF-prac/picoCTF/c3/original.py') as f:
+with open('D:/Coding/CTF-prac/picoCTF/c3/ori.py') as f:
     ciphertext = f.read()
-asciichars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmn"
+asciichars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmn1234567890"
 b = 1
 for i in range(len(ciphertext)):
     if i == b*b*b:
